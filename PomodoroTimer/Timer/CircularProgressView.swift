b2e4109918f26319359b5bbc73b4ecfb61c7537e
emirmalika <emirmalika0712@gmail.com>
2023-02-27 16:38:56 +0600

    replace storyboard with window on appDelegate

---
+++ PomodoroTimer/Timer/CircularProgressView.swift
@@ -0,0 +1,8 @@
+//
+//  CircularProgressView.swift
+//  PomodoroTimer
+//
+//  Created by Malik Em on 27.02.2023.
+//
+
+import Foundation
--- PomodoroTimer/Timer/TimerViewController.swift
+++ PomodoroTimer/Timer/TimerViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TimerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
